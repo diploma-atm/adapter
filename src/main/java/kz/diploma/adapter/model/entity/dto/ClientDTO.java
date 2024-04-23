a5cfb91616be4604cbd2c04b6b5761f6839f0449
justdika <fragerrr73@gmail.com>
2024-04-23 15:38:19 +0500

    add id to client response

--- src/main/java/kz/diploma/adapter/model/entity/dto/ClientDTO.java
+++ src/main/java/kz/diploma/adapter/model/entity/dto/ClientDTO.java
@@ -11,12 +11,14 @@ import java.util.Objects;
 @Getter
 @NoArgsConstructor
 public class ClientDTO {
+    public Integer id;
     public String surname;
     public String name;
     public String lastName;
     public String phoneNumber;
 
     public ClientDTO(ClientEntity clientEntity){
+        this.id = clientEntity.id;
         this.surname = clientEntity.surname;
         this.name = clientEntity.name;
         if(!Objects.isNull(clientEntity.lastName)){
