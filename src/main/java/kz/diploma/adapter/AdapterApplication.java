14bb3d8b4985ed066933a19eaec414974c816133
justdika <fragerrr73@gmail.com>
2024-04-28 14:14:32 +0500

    Import configs

--- src/main/java/kz/diploma/adapter/AdapterApplication.java
+++ src/main/java/kz/diploma/adapter/AdapterApplication.java
@@ -1,6 +1,8 @@
 package kz.diploma.adapter;
 
+import kz.diploma.library.shared.error_handling.ErrorHandlingAutoConfiguration;
 import kz.diploma.library.shared.jooq.JooqAutoConfiguration;
+import kz.diploma.library.shared.model.ModelsAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,7 +10,7 @@ import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@Import(value = {JooqAutoConfiguration.class})
+@Import(value = {JooqAutoConfiguration.class, ErrorHandlingAutoConfiguration.class, ModelsAutoConfiguration.class})
 public class AdapterApplication {
 
 	public static void main(String[] args) {
